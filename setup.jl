62d199303532262a043a3d0867a3249d36e5100a
Jason Eckstein <jekyllstein@gmail.com>
2019-06-18 10:53:30 -0600

    Update data download link in setup.jl

--- setup.jl
+++ setup.jl
@@ -6,6 +6,6 @@ Pkg.activate(@__DIR__)
 #instantiate environment
 Pkg.instantiate()
 
-download("www.kaggle.com/jekyllstein/ib-short-avail", joinpath(@__DIR__, "short_avail.zip"))
+download("https://www.dropbox.com/s/gwgxvno5ukwgohp/short_avail.zip?dl=0", joinpath(@__DIR__, "short_avail.zip"))
 
 println("Please extract the zip file short_avail.zip in the directory $@__DIR__")
